4e10ffd53f6bf390f0175a8055030cba9be8d78e
marvinick <marviluw@gmail.com>
2015-03-24 20:07:19 -0500

    added task_spec.rb

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -1,8 +1,18 @@
 require 'rails_helper'
 
 describe Project do
-  it "considers project without task to be done" do
-    project = Project.new
-    expect(project.done?).to be_truthy
+
+  describe "initialization" do
+    let(:project) { Project.new }
+    let(:task) { Task.new }
+
+    it "considers project without task to be done" do
+      expect(project.done?).to be_truthy
+    end
+
+    it "knows that a project with an incomplete task is not done" do
+      project.tasks << task
+      expect(project.done?). to be_falsy
+    end
   end
 end
---
+++ spec/models/task_spec.rb
@@ -0,0 +1,11 @@
+require "rails_helper"
+
+describe Task do
+  it "can distinguish a completed task" do
+    task = Task.new
+    expect(task).not_to be_complete
+    task.mark_completed
+    expect(task).to be_complete
+  end
+end
+
