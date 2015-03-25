3beae6696ee1ee702faba325353a9685c318845a
marvinick <marviluw@gmail.com>
2015-03-24 20:37:52 -0500

    added test completed tasks

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,15 @@
 class Task
+  attr_accessor :size, :completed
+  def initialize(options = {} )
+    @completed = options[:completed]
+    @size = options[:size]
+  end
+
+  def mark_completed
+    @completed = true
+  end
+
+  def complete?
+    @completed
+  end
 end
--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -14,5 +14,30 @@ describe Project do
       project.tasks << task
       expect(project.done?). to be_falsy
     end
+
+    it "marks a project done if its tasks are done" do
+      project.tasks << task
+      task.mark_completed
+      expect(project).to be_done
+    end
+  end
+end
+
+describe "estimates" do
+  let(:project) { Project.new }
+  let(:done) { Task.new(size: 2, completed: true) }
+  let(:small_not_done) { Task.new(size: 1) }
+  let(:large_not_done) { Task.new(size: 4) }
+
+  before(:example) do
+    project.tasks = [done, small_not_done, large_not_done]
+  end
+
+  it "can calculate total size" do
+    expect(project.total_size).to eq(7)
+  end
+
+  it "can calculate remaining size" do
+    expect(project.remaining_size).to eq(5)
   end
 end
